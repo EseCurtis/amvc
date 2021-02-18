a889225ac7808c226c72415c2166fbfa51c5f146
Ese Curtis <47295790+EseCurtis@users.noreply.github.com>
2021-02-18 22:36:36 +0000

    Delete content directory

--- content/views/home.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $render = new Render();
-    $render->prop("prop.php", ["hello"=>"hi"]); 
-    $DOM    = new DOM(); 
-?>
-<form action="<?=_URL_?>/core/php/amvc.api.php" callback="alert" async="true" method="POST">
-    <?=$DOM->interaction_api("api1.php")?>
-    <input type="submit" value="save">
-</form>
